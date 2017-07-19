ff85881af4f52ba637d63b56273cd08c0a06f877
Vadim.Zhiltsov <vadim.zhiltsov@mail.ru>
2017-07-19 19:34:46 +0300

    initial

--- main.js
+++ main.js
@@ -1,60 +1,98 @@
 const electron = require('electron')
-// Module to control application life.
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
+const http = require('http')
+const express = require('express')
+const windowStateKeeper = require('electron-window-state');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let mainWindowState
+
+
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  return new Promise((resolve) => {
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 450,
+      'x': mainWindowState.x,
+      'y': mainWindowState.y,
+      'width': mainWindowState.width,
+      'height': mainWindowState.height,
+      frame: false,
+      show: false,
+      title: 'Portable youtube player',
+      titleBarStyle: 'hidden',
+      icon: path.join(__dirname, 'youtube.png')
+    })
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+
+    //mainWindow.setOverlayIcon(path.join(__dirname, 'youtube.ico'))
+
+
+    mainWindow.setAlwaysOnTop(true);
+
+    mainWindow.setAspectRatio(16/9);
+
+    mainWindow.on('closed', function () {
+      mainWindow = null
+    });
+
+    mainWindow.once('ready-to-show', () => {
+      resolve();
+    })
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+    mainWindowState = windowStateKeeper({
+      defaultWidth: 800,
+      defaultHeight: 450
+    });
 
-// Quit when all windows are closed.
+    createWebServer();
+    createWindow();
+
+    mainWindowState.manage(mainWindow);
+});
+
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
+
+async function playNew(videoId) {
+  console.log('CREATING');
+
+  if (!mainWindow.isVisible()) {
+    mainWindow.show();
   }
-})
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  mainWindow.webContents.send('play-new', videoId);
+}
+
+
+function createWebServer() {
+  const ws = express()
+
+  ws.get('/play/:videoId', function (req, res) {
+    playNew(req.param('videoId'));
+    res.send('OK!');
+  })
+
+  ws.listen(3010, function () {
+    console.log('Example app listening on port 3010!')
+  })
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,37 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var ipc = require('electron').ipcRenderer;
+
+document.querySelector('#video').innerHTML = "hahah"
+
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+
+ipc.on('play-new', function (event, videoId) {
+  console.log(arguments);
+    play(videoId);
+});
+
+window.onYouTubePlayerAPIReady = () => {
+
+}
+
+function play(videoId) {
+  if (player) {
+    player.destroy();
+  }
+
+  player = new YT.Player('video', {
+    videoId: videoId,
+    playerVars: { autoplay: 1, rel: 0, modestbranding: 1  },
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    ipc.send('dom-ready');
+});
